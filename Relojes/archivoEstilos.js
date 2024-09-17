f976b69cbb483c9653e70f3ccc9582079a7a8c0c
marcos tonfor <94049496+marcostonfor@users.noreply.github.com>
2024-09-17 23:40:58 +0200

    Delete Relojes directory

--- Relojes/archivoEstilos.js
+++
@@ -1,8 +0,0 @@
-export const estilosPersonales = `
-    #reloj12h {
-    box-shadow: inset 0 0 10px 10px hsla(0, 0%, 41%, 0.397);
-    font-family: 'Comic Sans MS';
-    color: crimson;
-    text-shadow: 0.1vh 0.1vw 1px orange;
-    }
-    `;
